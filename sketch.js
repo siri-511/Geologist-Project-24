e4ebe9ce3f21e5ae27b29d1cfcb0b6d5a5475599
siri-511 <81082157+siri-511@users.noreply.github.com>
2021-05-06 18:51:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer,stone,iron,rubber;
+var sand1,sand2,sand3,sand4,sand5;
+var sand6,sand7,sand8,sand9,sand10;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    stone = new Stone(700,310,100,100);
+    iron = new Iron(310,340);
+    rubber = new Rubber (900,460,60);
+
+    sand1 = new Sand(500,450,10);
+    sand2 = new Sand(520,450,10);
+    sand3 = new Sand(525,450,10);
+    sand4 = new Sand(530,450,10);
+    sand5 = new Sand(535,450,10);
+    sand6 = new Sand(540,450,10);
+    sand7 = new Sand (545,450,10);
+    sand8 = new Sand(550,450,10);
+    sand9 = new Sand (555,450,10);
+    sand10 = new Sand(560,450,10);
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    iron.display();
+    stone.display();
+    rubber.display();
+    sand1.display();
+    sand2.display();
+    sand3.display();
+    sand4.display();
+    sand5.display();
+    sand6.display();
+    sand7.display();
+    sand8.display();
+    sand9.display();
+    sand10.display();
+ 
+}
